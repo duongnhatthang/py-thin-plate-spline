3cba0835c3ab0510dda136345c7830a44962256d
Thangdn3 <thangdn3@vinai.io>
2022-05-09 17:24:39 +0700

    add clean shadow gen code

---
+++ to_local.sh
@@ -0,0 +1,1 @@
+rsync -av -h -e ssh rtx_haiduong:/home/ubuntu/py-thin-plate-spline/ ../py-thin-plate-spline/
---
+++ to_server.sh
@@ -0,0 +1,1 @@
+rsync -av -h ../py-thin-plate-spline/ -e ssh rtx_haiduong:/home/ubuntu/py-thin-plate-spline/
